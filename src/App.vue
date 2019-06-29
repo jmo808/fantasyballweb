2e9752552a7b02b66a5c7697de219f5c8475bd84
Jules Ouellette <jules.ouellette@outlook.com>
2019-06-29 10:11:56 -0600

    more fixes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <v-app>
-
     <core-toolbar />
 
     <core-drawer />
--- src/components/core/Drawer.vue
+++ src/components/core/Drawer.vue
@@ -34,15 +34,6 @@
         </v-list-tile>
         <v-divider/>
         <v-list-tile
-          v-if="responsive"
-        >
-          <v-text-field
-            class="purple-input search-input"
-            label="Search..."
-            color="purple"
-          />
-        </v-list-tile>
-        <v-list-tile
           v-for="(link, i) in links"
           :key="i"
           :to="link.to"
